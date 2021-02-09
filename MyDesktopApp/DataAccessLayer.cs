8e30706d1de4d31ac812de96121e99e615bd991c
Idacarlsson <57900909+Idacarlsson@users.noreply.github.com>
2021-02-09 23:03:49 +0100

    DAL

---
+++ MyDesktopApp/DataAccessLayer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDesktopApp
+
+{
+    class DataAccessLayer
+    {
+        private string connectionString = "Server=localhost;Database=DB_Grupp12;User=IS12; Password=Grupp12.fmmi!";
+
+        
+        public static void AddUser(String str)
+        {
+
+        }
+
+        public static void DeleteUser(String str)
+        {
+
+        }
+
+        public static void UpdateUser(String str)
+        {
+
+        }
+
+        public static int CreateSchedule(int i)
+        {
+            return 0;
+        }
+
+        public static void UpdateSchedule(int i)
+        {
+            
+        }
+
+
+    }
+    
+}
