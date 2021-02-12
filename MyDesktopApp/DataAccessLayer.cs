b7d89d46071f9e780d4bbdbc52aebb1e6b2cc11f
Idacarlsson <57900909+Idacarlsson@users.noreply.github.com>
2021-02-12 12:31:49 +0100

    Dal

--- MyDesktopApp/DataAccessLayer.cs
+++ MyDesktopApp/DataAccessLayer.cs
@@ -10,21 +10,43 @@ namespace MyDesktopApp
 
 {
     class DataAccessLayer
-    {
+    { 
         private static string connectionString = "Integrated Security = True;server=localhost;Trusted_Connection=yes;Database=Grupp12_DB";
-
+        
         public static int [] AddUser(string username, string name, string surename, int totalIncome, int fixedCost, int variableCost, int savingGoal, int durationAmount)
         {
-            string test = "INSERT INTO Account(username, name, surename) Values ('mat', 'mats', 'mats')";
-            UpdateDB(test);
 
-            string query1 = "INSERT INTO Account(username, name, surename) Values ('" + username + "','" + name + "','" + surename + "')";
-            string query2 = "INSERT INTO SavingSchedule(accountUsername, totalIncome, fixedCost, variableCost,savingGoal, savingDuration) Values ('" + username + "','" + totalIncome + "','" + fixedCost + "','" + variableCost + "','" + savingGoal + "')";
-            UpdateDB(query1);
-            UpdateDB(query2);
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            SqlCommand sqlCommand= new SqlCommand("INSERT INTO Account VALUES(@username, @name, @surename)");
+            sqlCommand.Parameters.AddWithValue("@table","Account");
+            sqlCommand.Parameters.AddWithValue("@username",username);
+            sqlCommand.Parameters.AddWithValue("@name",name);
+            sqlCommand.Parameters.AddWithValue("@surename",surename);
+
+            SqlCommand sqlCommand1= new SqlCommand("INSERT INTO SavingSchedule VALUES(@accountUsername,@totalIncome, @fixedCost, @variableCost, @savingGoal)");
+            sqlCommand1.Parameters.AddWithValue("@table","SavingSchedule");
+            sqlCommand1.Parameters.AddWithValue("@accountUsername",username);
+            sqlCommand1.Parameters.AddWithValue("@totalIncome",totalIncome);
+            sqlCommand1.Parameters.AddWithValue("@fixedCost",fixedCost);
+            sqlCommand1.Parameters.AddWithValue("@variableCost",variableCost);
+            sqlCommand1.Parameters.AddWithValue("@durationAmount",durationAmount);
+
+            sqlCommand.Connection = sqlConnection;
 
             int []createSchedule = CreateSchedule(totalIncome, fixedCost, variableCost, savingGoal, durationAmount);
-           
+
+            try 
+            {
+                sqlConnection.Open();
+
+                sqlCommand.EndExecuteNonQuery();
+
+            }
+            catch(SqlException ex)
+            {
+                ErrorHandler.HandleException(ex);
+            }     
             return createSchedule;
         }
 
@@ -68,26 +90,15 @@ namespace MyDesktopApp
 
         }
 
-        public ArrayList<string> getAllAccounts() {
-            ArrayList<string> allAccounts = new ArrayList<string>();
+     
 
-            string query = "SELECT username FROM Account";
-           // ny metod där vi kan hämta från databasen
-
-
-
-        }
-
-        public int CreateSchedule(int i)
-        {
-            return 0;
-        }
 
         public void UpdateSchedule(int i)
         {
 
         }
-
+        
+       
         public static void UpdateDB(string query)
         {
             using (SqlConnection sqlConnection = new SqlConnection(connectionString))
