635ca0a355f2dba8b2dcc835e22585b37f86b0ac
Idacarlsson <57900909+Idacarlsson@users.noreply.github.com>
2021-02-16 18:24:04 +0100

    Assignment3

---
+++ WSAssignment3/Assignment3Service.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="Assignment3Service.asmx.cs" Class="WSAssignment3.Assignment3Service" %>
---
+++ WSClient_Assignment3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSClient_Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assignment3ServiceReference.Assignment3ServiceSoapClient client = new Assignment3ServiceReference.Assignment3ServiceSoapClient();
+            string str = client.HelloWorld();
+            string hej = client.HelloWld();
+
+            Console.WriteLine(str);
+            Console.WriteLine(hej);
+            Console.ReadLine();
+        }
+    }
+}
