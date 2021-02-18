eb7c2ce4fe2d9a9026ade89d53456213966608ed
Idacarlsson <57900909+Idacarlsson@users.noreply.github.com>
2021-02-18 11:14:41 +0100

    Client2

---
+++ WSClient2_Assignment3/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WSClient2_Assignment3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.getFileButton = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // getFileButton
+            // 
+            this.getFileButton.Location = new System.Drawing.Point(145, 244);
+            this.getFileButton.Name = "getFileButton";
+            this.getFileButton.Size = new System.Drawing.Size(75, 23);
+            this.getFileButton.TabIndex = 0;
+            this.getFileButton.Text = "Get file";
+            this.getFileButton.UseVisualStyleBackColor = true;
+            this.getFileButton.Click += new System.EventHandler(this.getFileButton_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(51, 32);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.Size = new System.Drawing.Size(270, 206);
+            this.textBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 309);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.getFileButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button getFileButton;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
---
+++ WSClient2_Assignment3/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WSClient2_Assignment3
+{
+    public partial class Form1 : Form
+
+    {
+        private Assignment3ServiceReference.Assignment3ServiceSoapClient client;
+        public Form1()
+        {
+            InitializeComponent();
+            client = new Assignment3ServiceReference.Assignment3ServiceSoapClient();
+        }
+
+        private void getFileButton_Click(object sender, EventArgs e)
+        {
+           
+            textBox.Text = client.TxtFile("C:\\Users\\Administrator\\Desktop\\HelloWorld.txt");
+
+        }
+    }
+}
+// denna följer MVC kolla föreläsningen om detta!!
+
