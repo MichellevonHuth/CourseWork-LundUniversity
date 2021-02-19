03cef01f2fcec299fa16119c1da8eca1ed4876a8
MichellevonHuth <michelle.vonhuth@gmail.com>
2021-02-19 09:38:04 +0000

    DAL assignment 4 client 1

---
+++ WSAssignment4/DataAccessLayer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WSAssignment4
+{
+        public class DataAccessLayer
+        {
+            private static string connectionString = "Integrated Security = True;server=localhost;Trusted_Connection=yes;Database=DB_Grupp12";
+
+            public void InsertAccount(string username, string name, string surename)
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand sqlCommand = new SqlCommand("INSERT INTO Account VALUES(@username, @name, @surename)"))
+                    {
+                        sqlCommand.Parameters.AddWithValue("@username", username);
+                        sqlCommand.Parameters.AddWithValue("@name", name);
+                        sqlCommand.Parameters.AddWithValue("@surename", surename);
+
+                        sqlConnection.Open();
+                        sqlCommand.ExecuteNonQuery();
+
+                    }
+                }
+            }
+        }
+}
+
