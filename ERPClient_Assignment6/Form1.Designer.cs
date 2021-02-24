7c95e30f1f54774bcc343364ac1a68b7f453f9d1
MichellevonHuth <michelle.vonhuth@gmail.com>
2021-02-24 15:36:02 +0000

    assignment6 næstan klar

--- ERPClient_Assignment6/Form1.Designer.cs
+++ ERPClient_Assignment6/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.outputTextBox = new System.Windows.Forms.TextBox();
             this.buttonEmployeeTables = new System.Windows.Forms.Button();
             this.buttonEmployeeRelative = new System.Windows.Forms.Button();
             this.buttonEmployeeAbsent = new System.Windows.Forms.Button();
@@ -44,49 +43,48 @@
             this.labelEmployeeColumns = new System.Windows.Forms.Label();
             this.buttonEmployeeColumns1 = new System.Windows.Forms.Button();
             this.buttonEmployeeColumns2 = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
-            // 
-            // outputTextBox
-            // 
-            this.outputTextBox.Location = new System.Drawing.Point(223, 12);
-            this.outputTextBox.Multiline = true;
-            this.outputTextBox.Name = "outputTextBox";
-            this.outputTextBox.Size = new System.Drawing.Size(360, 382);
-            this.outputTextBox.TabIndex = 0;
-            this.outputTextBox.TextChanged += new System.EventHandler(this.outputTextBox_TextChanged);
             // 
             // buttonEmployeeTables
             // 
-            this.buttonEmployeeTables.Location = new System.Drawing.Point(8, 78);
+            this.buttonEmployeeTables.Location = new System.Drawing.Point(16, 150);
+            this.buttonEmployeeTables.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.buttonEmployeeTables.Name = "buttonEmployeeTables";
-            this.buttonEmployeeTables.Size = new System.Drawing.Size(209, 23);
+            this.buttonEmployeeTables.Size = new System.Drawing.Size(418, 44);
             this.buttonEmployeeTables.TabIndex = 1;
             this.buttonEmployeeTables.Text = "Employee tables";
             this.buttonEmployeeTables.UseVisualStyleBackColor = true;
+            this.buttonEmployeeTables.Click += new System.EventHandler(this.buttonEmployeeTables_Click);
             // 
             // buttonEmployeeRelative
             // 
-            this.buttonEmployeeRelative.Location = new System.Drawing.Point(8, 149);
+            this.buttonEmployeeRelative.Location = new System.Drawing.Point(16, 287);
+            this.buttonEmployeeRelative.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.buttonEmployeeRelative.Name = "buttonEmployeeRelative";
-            this.buttonEmployeeRelative.Size = new System.Drawing.Size(209, 23);
+            this.buttonEmployeeRelative.Size = new System.Drawing.Size(418, 44);
             this.buttonEmployeeRelative.TabIndex = 2;
             this.buttonEmployeeRelative.Text = "Employees relatives";
             this.buttonEmployeeRelative.UseVisualStyleBackColor = true;
+            this.buttonEmployeeRelative.Click += new System.EventHandler(this.buttonEmployeeRelative_Click);
             // 
             // buttonEmployeeAbsent
             // 
-            this.buttonEmployeeAbsent.Location = new System.Drawing.Point(8, 212);
+            this.buttonEmployeeAbsent.Location = new System.Drawing.Point(16, 408);
+            this.buttonEmployeeAbsent.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.buttonEmployeeAbsent.Name = "buttonEmployeeAbsent";
-            this.buttonEmployeeAbsent.Size = new System.Drawing.Size(209, 23);
+            this.buttonEmployeeAbsent.Size = new System.Drawing.Size(418, 44);
             this.buttonEmployeeAbsent.TabIndex = 3;
             this.buttonEmployeeAbsent.Text = "Employee absent due to sickness";
             this.buttonEmployeeAbsent.UseVisualStyleBackColor = true;
             // 
             // buttonEmployeeMostAbsent
             // 
-            this.buttonEmployeeMostAbsent.Location = new System.Drawing.Point(8, 287);
+            this.buttonEmployeeMostAbsent.Location = new System.Drawing.Point(16, 552);
+            this.buttonEmployeeMostAbsent.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.buttonEmployeeMostAbsent.Name = "buttonEmployeeMostAbsent";
-            this.buttonEmployeeMostAbsent.Size = new System.Drawing.Size(209, 23);
+            this.buttonEmployeeMostAbsent.Size = new System.Drawing.Size(418, 44);
             this.buttonEmployeeMostAbsent.TabIndex = 4;
             this.buttonEmployeeMostAbsent.Text = "Most absent employee";
             this.buttonEmployeeMostAbsent.UseVisualStyleBackColor = true;
@@ -94,44 +92,49 @@
             // labelInformation
             // 
             this.labelInformation.AutoSize = true;
-            this.labelInformation.Location = new System.Drawing.Point(57, 36);
+            this.labelInformation.Location = new System.Drawing.Point(114, 69);
+            this.labelInformation.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.labelInformation.Name = "labelInformation";
-            this.labelInformation.Size = new System.Drawing.Size(111, 13);
+            this.labelInformation.Size = new System.Drawing.Size(224, 25);
             this.labelInformation.TabIndex = 5;
             this.labelInformation.Text = "Get information about:";
             // 
             // labelGetMetaData
             // 
             this.labelGetMetaData.AutoSize = true;
-            this.labelGetMetaData.Location = new System.Drawing.Point(631, 36);
+            this.labelGetMetaData.Location = new System.Drawing.Point(1262, 69);
+            this.labelGetMetaData.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.labelGetMetaData.Name = "labelGetMetaData";
-            this.labelGetMetaData.Size = new System.Drawing.Size(104, 13);
+            this.labelGetMetaData.Size = new System.Drawing.Size(207, 25);
             this.labelGetMetaData.TabIndex = 6;
             this.labelGetMetaData.Text = "Get metadata about:";
             // 
             // buttonAllKeys
             // 
-            this.buttonAllKeys.Location = new System.Drawing.Point(620, 62);
+            this.buttonAllKeys.Location = new System.Drawing.Point(1240, 119);
+            this.buttonAllKeys.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.buttonAllKeys.Name = "buttonAllKeys";
-            this.buttonAllKeys.Size = new System.Drawing.Size(133, 23);
+            this.buttonAllKeys.Size = new System.Drawing.Size(266, 44);
             this.buttonAllKeys.TabIndex = 7;
             this.buttonAllKeys.Text = "All keys";
             this.buttonAllKeys.UseVisualStyleBackColor = true;
             // 
             // buttonAllIndexes
             // 
-            this.buttonAllIndexes.Location = new System.Drawing.Point(620, 91);
+            this.buttonAllIndexes.Location = new System.Drawing.Point(1240, 175);
+            this.buttonAllIndexes.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.buttonAllIndexes.Name = "buttonAllIndexes";
-            this.buttonAllIndexes.Size = new System.Drawing.Size(133, 23);
+            this.buttonAllIndexes.Size = new System.Drawing.Size(266, 44);
             this.buttonAllIndexes.TabIndex = 8;
             this.buttonAllIndexes.Text = "All indexes";
             this.buttonAllIndexes.UseVisualStyleBackColor = true;
             // 
             // buttonAllTablesConstraints
             // 
-            this.buttonAllTablesConstraints.Location = new System.Drawing.Point(620, 120);
+            this.buttonAllTablesConstraints.Location = new System.Drawing.Point(1240, 231);
+            this.buttonAllTablesConstraints.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.buttonAllTablesConstraints.Name = "buttonAllTablesConstraints";
-            this.buttonAllTablesConstraints.Size = new System.Drawing.Size(133, 23);
+            this.buttonAllTablesConstraints.Size = new System.Drawing.Size(266, 44);
             this.buttonAllTablesConstraints.TabIndex = 9;
             this.buttonAllTablesConstraints.Text = "All tables constraint";
             this.buttonAllTablesConstraints.UseVisualStyleBackColor = true;
@@ -139,26 +142,29 @@
             // labelAllTables
             // 
             this.labelAllTables.AutoSize = true;
-            this.labelAllTables.Location = new System.Drawing.Point(647, 159);
+            this.labelAllTables.Location = new System.Drawing.Point(1294, 306);
+            this.labelAllTables.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.labelAllTables.Name = "labelAllTables";
-            this.labelAllTables.Size = new System.Drawing.Size(71, 13);
+            this.labelAllTables.Size = new System.Drawing.Size(144, 25);
             this.labelAllTables.TabIndex = 11;
             this.labelAllTables.Text = "Get all tables ";
             // 
             // buttonAllTables1
             // 
-            this.buttonAllTables1.Location = new System.Drawing.Point(627, 175);
+            this.buttonAllTables1.Location = new System.Drawing.Point(1254, 337);
+            this.buttonAllTables1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.buttonAllTables1.Name = "buttonAllTables1";
-            this.buttonAllTables1.Size = new System.Drawing.Size(108, 27);
+            this.buttonAllTables1.Size = new System.Drawing.Size(216, 52);
             this.buttonAllTables1.TabIndex = 12;
             this.buttonAllTables1.Text = "Alternative 1";
             this.buttonAllTables1.UseVisualStyleBackColor = true;
             // 
             // buttonAllTables2
             // 
-            this.buttonAllTables2.Location = new System.Drawing.Point(627, 212);
+            this.buttonAllTables2.Location = new System.Drawing.Point(1254, 408);
+            this.buttonAllTables2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.buttonAllTables2.Name = "buttonAllTables2";
-            this.buttonAllTables2.Size = new System.Drawing.Size(108, 27);
+            this.buttonAllTables2.Size = new System.Drawing.Size(216, 52);
             this.buttonAllTables2.TabIndex = 13;
             this.buttonAllTables2.Text = "Alternative 2";
             this.buttonAllTables2.UseVisualStyleBackColor = true;
@@ -166,35 +172,51 @@
             // labelEmployeeColumns
             // 
             this.labelEmployeeColumns.AutoSize = true;
-            this.labelEmployeeColumns.Location = new System.Drawing.Point(589, 252);
+            this.labelEmployeeColumns.Location = new System.Drawing.Point(1180, 539);
+            this.labelEmployeeColumns.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.labelEmployeeColumns.Name = "labelEmployeeColumns";
-            this.labelEmployeeColumns.Size = new System.Drawing.Size(188, 13);
+            this.labelEmployeeColumns.Size = new System.Drawing.Size(383, 25);
             this.labelEmployeeColumns.TabIndex = 14;
             this.labelEmployeeColumns.Text = "Get all columns of the employee tables";
             // 
             // buttonEmployeeColumns1
             // 
-            this.buttonEmployeeColumns1.Location = new System.Drawing.Point(627, 268);
+            this.buttonEmployeeColumns1.Location = new System.Drawing.Point(1254, 588);
+            this.buttonEmployeeColumns1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.buttonEmployeeColumns1.Name = "buttonEmployeeColumns1";
-            this.buttonEmployeeColumns1.Size = new System.Drawing.Size(108, 27);
+            this.buttonEmployeeColumns1.Size = new System.Drawing.Size(216, 52);
             this.buttonEmployeeColumns1.TabIndex = 15;
             this.buttonEmployeeColumns1.Text = "Alternative 1";
             this.buttonEmployeeColumns1.UseVisualStyleBackColor = true;
             // 
             // buttonEmployeeColumns2
             // 
-            this.buttonEmployeeColumns2.Location = new System.Drawing.Point(627, 301);
+            this.buttonEmployeeColumns2.Location = new System.Drawing.Point(1254, 652);
+            this.buttonEmployeeColumns2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.buttonEmployeeColumns2.Name = "buttonEmployeeColumns2";
-            this.buttonEmployeeColumns2.Size = new System.Drawing.Size(108, 27);
+            this.buttonEmployeeColumns2.Size = new System.Drawing.Size(216, 52);
             this.buttonEmployeeColumns2.TabIndex = 16;
             this.buttonEmployeeColumns2.Text = "Alternative 2";
             this.buttonEmployeeColumns2.UseVisualStyleBackColor = true;
             // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(506, 69);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 33;
+            this.dataGridView.Size = new System.Drawing.Size(636, 694);
+            this.dataGridView.TabIndex = 17;
+            this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellContentClick);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 416);
+            this.ClientSize = new System.Drawing.Size(1600, 800);
+            this.Controls.Add(this.dataGridView);
             this.Controls.Add(this.buttonEmployeeColumns2);
             this.Controls.Add(this.buttonEmployeeColumns1);
             this.Controls.Add(this.labelEmployeeColumns);
@@ -210,17 +232,16 @@
             this.Controls.Add(this.buttonEmployeeAbsent);
             this.Controls.Add(this.buttonEmployeeRelative);
             this.Controls.Add(this.buttonEmployeeTables);
-            this.Controls.Add(this.outputTextBox);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox outputTextBox;
         private System.Windows.Forms.Button buttonEmployeeTables;
         private System.Windows.Forms.Button buttonEmployeeRelative;
         private System.Windows.Forms.Button buttonEmployeeAbsent;
@@ -236,6 +257,7 @@
         private System.Windows.Forms.Label labelEmployeeColumns;
         private System.Windows.Forms.Button buttonEmployeeColumns1;
         private System.Windows.Forms.Button buttonEmployeeColumns2;
+        private System.Windows.Forms.DataGridView dataGridView;
     }
 }
 
