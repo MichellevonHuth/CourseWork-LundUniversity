32b0f7fc643e0a6ea76d838864c1fd66b1879fc5
Idacarlsson <57900909+Idacarlsson@users.noreply.github.com>
2021-02-24 21:56:25 +0100

    form

--- ERPClient_Assignment6/Form1.cs
+++ ERPClient_Assignment6/Form1.cs
@@ -13,7 +13,7 @@ namespace ERPClient_Assignment6
     public partial class Form1 : Form
     {
         Assignment6Service proxy = new Assignment6Service();
- 
+
         public Form1()
         {
             InitializeComponent();
@@ -51,18 +51,159 @@ namespace ERPClient_Assignment6
 
         }
 
-        private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void buttonEmployeeAbsent_Click(object sender, EventArgs e)
         {
+            try
+            {
+                DataTable dt = proxy.EmployeeAbsent2004();
+                dataGridView.DataSource = dt;
+            }
+
+            catch (Exception ex)
+            {
+                string message = Errorhandler.HandleException(ex);
+                errorMessageLbl.Text = message;
+            }
+        }
+
+        private void buttonEmployeeMostAbsent_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable dt = proxy.EmployeeAbsentTheMost();
+                dataGridView.DataSource = dt;
+            }
+
+            catch (Exception ex)
+            {
+                string message = Errorhandler.HandleException(ex);
+                errorMessageLbl.Text = message;
+            }
 
         }
 
         private void buttonAllKeys_Click(object sender, EventArgs e)
         {
+            try
+            {
+                DataTable dt = proxy.AllKeys();
+                dataGridView.DataSource = dt;
+            }
+
+            catch (Exception ex)
+            {
+                string message = Errorhandler.HandleException(ex);
+                errorMessageLbl.Text = message;
+            }
 
         }
 
+        private void buttonAllIndexes_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable dt = proxy.AllIndexes();
+                dataGridView.DataSource = dt;
+            }
+
+            catch (Exception ex)
+            {
+                string message = Errorhandler.HandleException(ex);
+                errorMessageLbl.Text = message;
+            }
+
+        }
+
+        private void buttonAllTablesConstraints_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable dt = proxy.AllTableConstraints();
+                dataGridView.DataSource = dt;
+            }
+
+            catch (Exception ex)
+            {
+                string message = Errorhandler.HandleException(ex);
+                errorMessageLbl.Text = message;
+            }
+
+        }
+
+        private void buttonAllTables1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable dt = proxy.AllTables1();
+                dataGridView.DataSource = dt;
+            }
+
+            catch (Exception ex)
+            {
+                string message = Errorhandler.HandleException(ex);
+                errorMessageLbl.Text = message;
+            }
+
+        }
+
+        private void buttonAllTables2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable dt = proxy.AllTables2();
+                dataGridView.DataSource = dt;
+            }
+
+            catch (Exception ex)
+            {
+                string message = Errorhandler.HandleException(ex);
+                errorMessageLbl.Text = message;
+            }
+
+        }
+
+        private void buttonEmployeeColumns1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable dt = proxy.AllColumns1();
+                dataGridView.DataSource = dt;
+            }
+
+            catch (Exception ex)
+            {
+                string message = Errorhandler.HandleException(ex);
+                errorMessageLbl.Text = message;
+            }
+
+        }
+
+        private void buttonEmployeeColumns2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable dt = proxy.AllColumns2();
+                dataGridView.DataSource = dt;
+            }
+
+            catch (Exception ex)
+            {
+                string message = Errorhandler.HandleException(ex);
+                errorMessageLbl.Text = message;
+            }
+
+        }
+
+        private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+
+        }
+
+
         private void errorMessageLbl_Click(object sender, EventArgs e)
         {
+
 
         }
     }
